c7eb65185f33bbdaa83f5b8b988949b62d92e0f8
ozy <ozguryasaralhan@gmail.com>
2020-10-13 17:32:22 +0300

    While Loop with exit mechanism is added.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#This list contains equivalent letters to the morse. white space " " defined as "/" as equivalent of breaks between words
 morse_dict={
     "0":"-----",
     "1":".----",
@@ -46,24 +47,36 @@ print("""....................................................
 - Only English letters and numbers can be converted.
 ....................................................""")
 
-# expression is getting from user with lower characters.
-expression = input("").lower()   
+while True:
+    # expression is getting from user with lower characters.
+    expression = input("").lower()   
 
-# morse_expression is final expression (morse)
-morse_expression =  '' 
+    # morse_expression is final expression (morse)
+    morse_expression =  '' 
 
-#print("first expression: " + expression) #for debugging only
+    #print("first expression: " + expression) #for debugging only
 
-#In this loop we get every character from expression string and convert them to morse value which is matched on morse_dict list.
-for i in expression:
-    if i in morse_dict:
-        morse_expression = morse_expression + morse_dict[i] + " " #string concatenation
-        #print(morse_dict[i]) #for debugging only
+    #In this loop we get every character from expression string and convert them to morse value which is matched on morse_dict list.
+    for i in expression:
+        if i in morse_dict:
+            morse_expression = morse_expression + morse_dict[i] + " " #string concatenation
+            #print(morse_dict[i]) #for debugging only
 
-# morse_expression[:-1] it saves us from whitespace in end.
-morse_expression = morse_expression[:-1]    #morse_expression.rstrip(" ") #it doesnt work here, new solution is done
+    # morse_expression[:-1] it saves us from whitespace in end.
+    morse_expression = morse_expression[:-1]    #morse_expression.rstrip(" ") #it doesnt work here, new solution is done
 
-print(morse_expression) #for debugging only
-input("Press Enter for Exit.")
+    print(morse_expression) #for debugging only
+    check_exit = input("Do you want to translate morse again?(y/n)").lower()
+    if(check_exit == "y" or check_exit == "yes" ):
+        print("Write you expression please.")
+        pass
+    elif(check_exit == "n" or check_exit == "no"):
+        print("Thank you and goodbye.")
+        break
+print("........................")
+input("For exit press the Enter.")
+        
+
+    
 
 
